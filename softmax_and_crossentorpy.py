eaf85ff6096ee75b9b2aee24372980e91a6d342e
Oguzhan Gencer <dev.oguzhangencer@gmail.com>
2022-06-28 15:18:20 +0300

    cross_entropy

--- softmax_and_crossentorpy.py
+++ softmax_and_crossentorpy.py
@@ -13,3 +13,8 @@ x = torch.tensor([2.0, 1.0, 0.1])
 outputs = torch.softmax(x, dim=0)
 print('softmax torch: ', outputs)
 
+def cross_entropy(actual, predicted):
+    EPS = 1e-15
+    predicted = np.clip(predicted, EPS, 1 - EPS)
+    loss = -np.sum(actual * np.log(predicted))
+    return loss
