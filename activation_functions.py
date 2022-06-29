4354a02c0397fef74adcff7960aac3880d0c2230
Oguzhan Gencer <dev.oguzhangencer@gmail.com>
2022-06-29 22:25:41 +0300

    use_activation_functions

--- activation_functions.py
+++ activation_functions.py
@@ -49,3 +49,14 @@ class NeuralNet(nn.Module):
         out = self.sigmoid(out)
         return out
 
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(NeuralNet, self).__init__()
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, 1)
+
+    def forward(self, x):
+        out = torch.relu(self.linear1(x))
+        out = torch.sigmoid(self.linear2(out))
+        return out
