b10138d2041bba2be47a5d23ad2db43eb33d0e08
Oguzhan Gencer <dev.oguzhangencer@gmail.com>
2022-06-29 22:15:06 +0300

    create_nn_module

--- activation_functions.py
+++ activation_functions.py
@@ -33,3 +33,19 @@ print(output)
 lrelu = nn.LeakyReLU()
 output = lrelu(x)
 print(output)
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(NeuralNet, self).__init__()
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.linear2 = nn.Linear(hidden_size, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        out = self.linear1(x)
+        out = self.relu(out)
+        out = self.linear2(out)
+        out = self.sigmoid(out)
+        return out
+
