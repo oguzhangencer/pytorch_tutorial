5f5407dbed9b18507642a6a60636e56a2aac94e7
Oguzhan Gencer <dev.oguzhangencer@gmail.com>
2022-07-05 16:54:17 +0300

    loss_optimizer_trainmodel

--- feed_forward.py
+++ feed_forward.py
@@ -34,3 +34,57 @@ test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
                                           batch_size=batch_size,
                                           shuffle=False)
 
+examples = iter(test_loader)
+example_data, example_targets = examples.next()
+
+for i in range(6):
+    plt.subplot(2,3,i+1)
+    plt.imshow(example_data[i][0], cmap='gray')
+plt.show()
+
+
+# fully connected neural network with one hidden layer
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNet, self).__init__()
+        self.input_size = input_size
+        self.l1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.l2 = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        out = self.l1(x)
+        out = self.relu(out)
+        out = self.l2(out)
+        # no activation and no softmax at the end
+        return out
+
+
+model = NeuralNet(input_size, hidden_size, num_classes).to(device)
+
+# loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# train the model
+n_total_steps = len(train_loader)
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+
+        # origin shape: [100, 1, 28, 28]
+        # resized: [100, 784]
+        images = images.reshape(-1, 28 * 28).to(device)
+        labels = labels.to(device)
+
+        # forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        # backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i + 1) % 100 == 0:
+            print(f'Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{n_total_steps}], Loss: {loss.item():.4f}')
+
