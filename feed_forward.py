ecf4b1fb4a7f1fdbe28fd39a0f7ed0107f9c469f
Oguzhan Gencer <dev.oguzhangencer@gmail.com>
2022-07-05 16:49:53 +0300

    Device_configuration

--- feed_forward.py
+++ feed_forward.py
@@ -3,3 +3,7 @@ import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
