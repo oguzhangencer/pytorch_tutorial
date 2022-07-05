35a7427142817b8ff054b8f1b74a2e454092f432
Oguzhan Gencer <dev.oguzhangencer@gmail.com>
2022-07-05 16:51:07 +0300

    MNIST_dataset

--- feed_forward.py
+++ feed_forward.py
@@ -14,3 +14,13 @@ num_classes = 10
 num_epochs = 2
 batch_size = 100
 learning_rate = 0.001
+
+# MNIST dataset
+train_dataset = torchvision.datasets.MNIST(root='./data',
+                                           train=True,
+                                           transform=transforms.ToTensor(),
+                                           download=True)
+
+test_dataset = torchvision.datasets.MNIST(root='./data',
+                                          train=False,
+                                          transform=transforms.ToTensor())
