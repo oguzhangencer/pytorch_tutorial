6a4eb6cc18aad52de0cc62b537b334beb2b5c8a5
Oguzhan Gencer <dev.oguzhangencer@gmail.com>
2022-07-05 16:50:22 +0300

    hyper_parameters

--- feed_forward.py
+++ feed_forward.py
@@ -4,6 +4,13 @@ import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 
-# Device configuration
+# device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+# hyper-parameters
+input_size = 784 # 28x28
+hidden_size = 500
+num_classes = 10
+num_epochs = 2
+batch_size = 100
+learning_rate = 0.001
